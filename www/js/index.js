1c5bab8001c4ad28aa779b226cd156e4c30858ef
Akas Antony <antony.akasgmail.com>
2016-04-08 19:24:08 +0530

    modified init

--- www/js/index.js
+++ www/js/index.js
@@ -5,11 +5,27 @@ function timeOut(){
 }
 
 var app = {
-    initialize: function() {this.onDeviceReady()},
-    bindEvents: function() {var y = 0},
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
         timeOut();
+        console.log('Received Event: ' + id);
     }
 };
-
-app.initialize();
